acd654fb239010cff150c986cb3a2428ce53fdb5
0x0than <136307533+revothan@users.noreply.github.com>
2025-05-28 10:09:45 +0700

    Add main App component with routing

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Quiz from './pages/Quiz'
+import Result from './pages/Result'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-background">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
